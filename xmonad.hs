e115719f8a2a3e915a15e695c5912d0ab4268d70
Morten Brøns-Pedersen <mortenbp@gmail.com>
2011-11-07 15:30:59 +0100

    Xmonad searches

--- xmonad.hs
+++ xmonad.hs
@@ -2,11 +2,15 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
+-- Main inspiration:
+-- http://www.haskell.org/haskellwiki/Xmonad/Config_archive/Brent_Yorgey's_darcs_xmonad.hs
+
 import XMonad
 import XMonad.StackSet as W
 import Data.Map (unions, fromList)
-import Data.List (partition)
-import Control.Monad (liftM2)
+import Data.Maybe (isNothing, isJust, catMaybes)
+import Data.List (isPrefixOf, partition, (\\))
+import Control.Monad (liftM2, when)
 
 -- Chats a bit on dbus and sets {terminal = gnome-terminal}
 import XMonad.Config.Gnome
@@ -44,8 +48,6 @@ import XMonad.Actions.CycleWS
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.CycleWindows
 import XMonad.Actions.GridSelect
--- import MyGridSelect
-import XMonad.Actions.TopicSpace
 import XMonad.Actions.Submap        -- For different kind of searches
 import XMonad.Actions.Search
 import XMonad.Actions.WithAll       -- {withAll'} can be used to migrate to
@@ -70,7 +72,7 @@ import XMonad.Prompt.Input
 ----- Util
 import XMonad.Util.Run
 import XMonad.Util.Loggers
-import XMonad.Util.NamedScratchpad  -- This is quite nifty
+import XMonad.Util.EZConfig         -- "M-C-x" style keybindings
 
 myLayout = Tall 1 (3/100) (5/7) ||| simpleTabbedBottom
 
@@ -78,8 +80,7 @@ myManageHook =
   [ className =? "Do" --> doIgnore
   , className =? "Pidgin" --> doShift "im"
   , className =? "XChat" --> doShift "im"
-  , className =? "Bitcoin" --> doShift "scratchpad"
-  , resource =? "gmail" --> doShift "organise"
+  , className =? "Bitcoin" --> doShift "bitcoin"
   , title =? "Calendar" --> doShift "organise"
   , title =? "GMail" --> doShift "organise"
   , className =? "Gimp" --> viewShift "gimp"
@@ -94,14 +95,12 @@ myTopics =
   , "im"
   , "organise"
   , "reading"
-  , "study"
-  , "graphics"
+  , "inkscape"
   , "gimp"
   , "multimedia"
   , "procrastination"
   , "absalon"
   , "pwnies"
-  , "pwnies-util"
   , "idapro"
   , "virtualbox"
   , "download"
@@ -121,21 +120,26 @@ myTopics =
   , "bitcoin"
     -- Work
   , "ip"
-    -- Named scratch pad
-  , "scratchpad"
   ]
+
+myShell = "gnome-terminal"
+myBrowser = "chromium-browser"
+edit s = spawn ("emacs " ++ s)
+shell = spawn myShell
+browser s = spawn ("chromium-browser " ++ s)
+newBrowser s = spawn ("chromium-browser --new-window " ++ s)
 
 myTopicConfig = TopicConfig
   { topicDirs = fromList []
   , topicActions =
        fromList $
-       [ ("web", spawn "chromium-browser")
+       [ ("web", browser "")
        , ("im", spawn "pidgin" >>
                 spawn "xchat")
        , ("organise", spawn "gmail" >>
                       spawn "gcal")
        , ("multimedia", spawn "sonata")
-       , ("procrastination", spawn $ inNewBrowser
+       , ("procrastination", newBrowser
                              "facebook.com \
                              \xkcd.com \
                              \smbc-comics.com \
@@ -143,36 +147,32 @@ myTopicConfig = TopicConfig
        , ("idapro", spawn "idaq")
        , ("virtualbox", spawn "virtualbox")
        , ("reading", spawn "evince")
-       , ("emacs", spawn "emacs ~/.emacs.d/config/bindings.el")
-       , ("xmonad", spawn "emacs ~/config/xmonad.hs" >>
-                    spawn (inNewBrowser
-                    "http://xmonad.org/xmonad-docs/xmonad-contrib/index.html"))
-       , ("install", spawn "gnome-terminal")
-       , ("mylib", spawn "emacs MyLib.mlb" >>
-                   spawn "gnome-terminal")
-       , ("preml", spawn "emacs PreML.mlb" >>
-                   spawn "gnome-terminal")
-       , ("iptest", spawn "emacs main.sml")
-       , ("bitcoin", spawn "emacs mtgox.py newscalper.py" >>
+       , ("emacs", edit "~/.emacs.d/config/bindings.el")
+       , ("xmonad", edit "~/config/xmonad.hs" >>
+                    newBrowser
+                    "http://xmonad.org/xmonad-docs/xmonad-contrib/index.html")
+       , ("install", shell)
+       , ("mylib", edit "MyLib.mlb" >>
+                   shell)
+       , ("preml", edit "src/PreML.mlb" >>
+                   shell)
+       , ("iptest", edit "main.sml")
+       , ("bitcoin", edit "mtgox.py newscalper.py" >>
                      -- spawn "bitcoin" >>
                      spawn "gnome-terminal -x python ticker.py" >>
-                     spawn "gnome-terminal" >>
-                     spawn (inNewBrowser "eclipsemc.com mtgox.com \
-                                         \blockexplorer.com/q/estimate"))
-       , ("absalon", spawn $ inNewBrowser "punkt.ku.dk")
-       , ("ip", spawn "gnome-terminal" >>
-                spawn "emacs timer.txt")
+                     shell >>
+                     newBrowser "eclipsemc.com mtgox.com \
+                                \blockexplorer.com/q/estimate")
+       , ("absalon", browser "punkt.ku.dk")
+       , ("ip", edit "timer.txt")
        ]
-  , defaultTopicAction = const $ spawn "gnome-terminal"
-  -- , defaultTopicAction = const $ return ()
+  , defaultTopicAction = const $ shell
+  , defaultTopic = "web"
   }
-    where
-      inNewBrowser s = "chromium-browser --new-window " ++ s
 
 setWorkspaceDirs layout =
   onWorkspace "organise" (workspaceDir "~/notes" layout) $
   onWorkspace "pwnies" (workspaceDir "~/zomg-pwnies" layout) $
-  onWorkspace "pwnies-util" (workspaceDir "~/zomg-pwnies" layout) $
   onWorkspace "download" (workspaceDir "~/downloads" layout) $
   onWorkspace "mylib" (workspaceDir "~/code/sml/mylib" layout) $
   onWorkspace "preml" (workspaceDir "~/code/sml/preml" layout) $
@@ -180,59 +180,141 @@ setWorkspaceDirs layout =
   onWorkspace "study" (workspaceDir "~/study" layout) $
   onWorkspace "iptest" (workspaceDir "~/study/ip2011/test" layout) $
   onWorkspace "bitcoin" (workspaceDir "~/code/python/mtgox" layout) $
-  onWorkspace "ip" (workspaceDir "~/study/ip2011" layout) $
   onWorkspace "sml" (workspaceDir "~/code/sml" layout) $
   onWorkspace "haskell" (workspaceDir "~/code/haskell" layout) $
   onWorkspace "python" (workspaceDir "~/code/python" layout) $
+  onWorkspace "ip" (workspaceDir "~/study/ip2011" layout) $
   workspaceDir "~" layout
-
-myLogHook dest = dynamicLogWithPP defaultPP { ppOutput = hPutStrLn dest
-                                            , ppVisible = wrap "(" ")"
-                                            }
 
 
 br0nsConfig =
   gnomeConfig
-       { modMask = mod4Mask -- default is mod1Mask
+       { modMask = mod4Mask
        , manageHook = manageHook gnomeConfig <+>
                       manageDocks <+>
                       composeAll myManageHook
        , layoutHook = avoidStrutsOn [U] $
                       smartBorders $
-                      showWName $
                       setWorkspaceDirs myLayout
        , borderWidth = 0
-       , keys = myKeys
+       -- , keys = myKeys
        , focusFollowsMouse = False
        , logHook = do fadeInactiveLogHook 0.7
-                      -- myLogHook h
        , XMonad.workspaces = myTopics
        }
+       `additionalKeysP` myKeys
 
 main = do
-  -- h <- spawnPipe myDzen
   checkTopicConfig myTopics myTopicConfig
   xmonad $ br0nsConfig
 
-myKeys conf@(XConfig {modMask = modm}) =
-  unions
-  [ keys gnomeConfig conf
-  -- , planeKeys modm (Lines 3) Finite
-  , fromList $ myNewKeys modm
-  ]
+myKeys' conf@(XConfig {modMask = modm}) =
+  []
 
-myNewKeys modm =
+myRemoveKeys =
+  [ ((mod4Mask .|. shiftMask), n) | n <- [xK_1 .. xK_9]] ++
+  [ (mod4Mask, n)                 | n <- [xK_1 .. xK_9]] ++
+  []
+
+-- myKeys conf@(XConfig {modMask = modm}) =
+--   fromList
+--   -- CycleWindows
+--   [ ((modm, xK_s), cycleRecentWindows [xK_Super_L] xK_s xK_w)
+--   , ((modm, xK_g), goToSelected myGSConfig)
+--   , ((modm, xK_c), changeDir myXPConfig)
+--   -- Workspace navigation
+--   , ((modm .|. shiftMask, xK_z), shiftToSelectedWS False myGSConfig)
+--   , ((modm, xK_z), goToSelectedWS False myGSConfig)
+--   , ((modm .|. shiftMask, xK_a), shiftToSelectedWS True myGSConfig)
+--   , ((modm, xK_a), goToSelectedWS True myGSConfig)
+--   -- Dynamic workspaces
+--   , ((modm .|. shiftMask, xK_n), addWorkspacePrompt myXPConfig >>
+--                                  shell)
+--   , ((modm .|. shiftMask, xK_BackSpace), killAll >> myRemoveWorkspace)
+--   , ((modm .|. shiftMask, xK_r), renameWorkspace myXPConfig)
+--   , ((modm .|. shiftMask, xK_s), newScratchpad)
+--   , ((modm .|. shiftMask, xK_7), submap . mySearchMap $ myPromptSearch)
+--   , ((modm .|. shiftMask, xK_g), submap . mySearchMap $ mySelectSearch)
+--   ]
+
+myKeys =
   -- CycleWindows
-  [ ((modm, xK_s), cycleRecentWindows [xK_Super_L] xK_s xK_w)
-  , ((modm, xK_g), goToSelected myGSConfig)
-  , ((modm, xK_c), changeDir defaultXPConfig)
+  [ ("M-s", cycleRecentWindows [xK_Super_L] xK_s xK_w)
+  , ("M-g", goToSelected myGSConfig)
+  , ("M-c", changeDir myXPConfig)
   -- Workspace navigation
-  , ((modm .|. shiftMask, xK_z), shiftToSelectedWS False myGSConfig)
-  , ((modm, xK_z), goToSelectedWS False myGSConfig)
-  , ((modm .|. shiftMask, xK_a), shiftToSelectedWS True myGSConfig)
-  , ((modm, xK_a), goToSelectedWS True myGSConfig)
+  , ("M-S-z", shiftToSelectedWS False myGSConfig)
+  , ("M-z", goToSelectedWS False myGSConfig)
+  , ("M-S-a", shiftToSelectedWS True myGSConfig)
+  , ("M-a", goToSelectedWS True myGSConfig)
+  -- Dynamic workspaces
+  , ("M-S-n", addWorkspacePrompt myXPConfig >> shell)
+  , ("M-S-<Backspace>", killAll >> myRemoveWorkspace)
+  , ("M-S-r", renameWorkspace myXPConfig)
+  , ("M-S-s", newScratchpad)
+  , ("M-'", submap . mySearchMap $ myPromptSearch)
+  , ("M-C-'", submap . mySearchMap $ mySelectSearch)
   ]
 
+-- from XMonad.Actions.Search
+mySearchMap method = fromList $
+        [ ((0, xK_g), method google)
+        , ((0, xK_w), method wikipedia)
+        , ((0, xK_h), method hoogle)
+        , ((shiftMask, xK_h), method hackage)
+        , ((0, xK_s), method scholar)
+        , ((0, xK_m), method mathworld)
+        , ((0, xK_p), method maps)
+        , ((0, xK_d), method dictionary)
+        , ((0, xK_a), method alpha)
+        , ((0, xK_l), method lucky)
+        , ((0, xK_i), method images)
+        , ((shiftMask, xK_i), method imdb)
+        , ((0, xK_y), method youtube)
+        ]
+          where hackage =
+                  searchEngine "hackage" "http://www.google.dk/search?btnI&q=site%3Ahackage.haskell.org+"
+
+-- Prompt search: get input from the user via a prompt, then run the search in
+-- `myBrowser` and automatically switch to the "web" workspace
+myPromptSearch (SearchEngine _ site)
+  = inputPrompt myXPConfig "Search" ?+ search myBrowser site
+
+-- Select search: do a search based on the X selection
+mySelectSearch eng = selectSearch eng
+
+-- Switch to the "web" workspace
+viewWeb = windows (W.view "web")
+
+-- Remove workspace unless it's a topic
+myRemoveWorkspace :: X ()
+myRemoveWorkspace = do
+  s <- gets windowset
+  case s of
+    StackSet {current = W.Screen { workspace = Workspace { tag = this } } } ->
+      when (this `notElem`myTopics) removeWorkspace
+
+-- Create a new workspace named "scratchpadX"
+newScratchpad :: X ()
+newScratchpad = withWindowSet $ \w -> do
+  let wss = W.workspaces w
+      cws = map W.tag $ Prelude.filter
+            (\ws -> "scratchpad" `isPrefixOf`
+                    W.tag ws && isJust (W.stack ws)) wss
+      num = head $ [0..] \\
+            catMaybes (map (readMaybe . drop (length "scratchpad")) cws)
+      new = "scratchpad" ++ show num
+  when (new `notElem` (map W.tag wss)) $ addWorkspace new
+  windows $ W.view new
+  shell
+ where readMaybe s = case reads s of
+                       [(r,_)] -> Just r
+                       _       -> Nothing
+
+-- Nizzle colours
+myXPConfig = defaultXPConfig
+  { fgColor = "#a8a3f7"
+  , bgColor = "#3f3c6d"}
 
 ----- Extension on GridSelect
 myGSConfig = defaultGSConfig {gs_navigate = navNSearch}
