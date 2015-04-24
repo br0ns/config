74a5e2b39e0d4971894d8667a025209d66d2b45b
Morten Brøns-Pedersen <f@ntast.dk>
2015-04-24 16:45:01 +0200

    Moved everything to VCSH

--- packagelist.sh
+++
@@ -1,4 +0,0 @@
-sort packagelist | sponge packagelist
-git add packagelist
-git commit -m "packagelist"
-git push
--- secret.sh
+++
@@ -1,6 +0,0 @@
-rm -f secret.tar secret.tar.gpg
-tar cfv secret.tar secret
-gpg -c secret.tar && rm secret.tar
-git add secret.tar.gpg
-git commit -m "secret"
-git push
